6d672a53539102ba5609a4183c46b1ecf258d025
James Song <skdmlrh12@gmail.com>
2023-12-20 23:49:52 +0900

    Feat: 댓글 창 react-quill로 구현

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import Input from '@/components/common/Input/Input';
+import TextArea from '@/components/common/TextArea';
 
 /**
  * 컴포넌트 실험용 페이지입니다. (컴포넌트 구현 완료 후 삭제 예정)
  */
-export default function Test() {
-  const [password, setPassword] = useState('');
 
+export default function Test() {
   return (
     <Div>
-      <Input type="email" />
-      <Input type="password" isPassword={true} setPassword={setPassword} />
-      <Input type="passwordConfirm" isPassword={true} passwordCheck={password} />
-      <Input type="title" />
-      <Input type="dueDate" />
-      <Input type="tag" />
+      <TextArea />
     </Div>
   );
 }
