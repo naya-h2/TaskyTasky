a8c7489eb83524580049d7a1cbfe4545d9c80fea
안희원 <hw7666@sogang.ac.kr>
2023-12-22 23:15:00 +0900

    Feat: API 데이터 type 파일 생성
    Co-authored-by: Lee Gyuho <leegyuho-programer@users.noreply.github.com>

--- lib/types/type.ts
+++ lib/types/type.ts
@@ -1,3 +1,22 @@
+// Auth
+
+export interface Auth {
+  user: User;
+  accessToken: string;
+}
+
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface Password {
+  password: string;
+  newPassword: string;
+}
+
+//Cards
+
 export interface Card {
   id: number;
   title: string;
@@ -16,49 +35,57 @@ export interface Card {
   updatedAt: string;
 }
 
-export interface Cards {
+export interface CreateCard {
+  assigneeUserId: number;
+  dashboardId: number;
+  columnId: number;
+  title: string;
+  description: string;
+  dueDate: string;
+  tags: string[];
+  imageUrl: string;
+}
+
+export interface CheckCard {
   cursorId: number;
   totalCount: number;
   cards: Card[];
 }
 
-export interface Invite {
+export interface ChangeCard {
+  assigneeUserId: number;
+  title: string;
+  description: string;
+  dueDate: string;
+  tags: string[];
+  imageUrl: string;
+}
+
+//Columns
+
+export interface Column {
   id: number;
-  inviterUserId: number;
+  title: string;
   teamId: string;
-  dashboard: {
-    title: string;
-    id: number;
-  };
-  invitee: {
-    nickname: string;
-    id: number;
-  };
-  inviteAccepted: boolean;
   createdAt: string;
   updatedAt: string;
 }
 
-export interface Invites {
-  cursorId: number;
-  invitations: Invite[];
+export interface CheckColumn {
+  result: 'SUCCESS'; // ???????????????
+  data: Column[];
 }
 
-export interface Members {
-  members: Member[];
-  totalCount: number;
+export interface ChangeColumn {
+  title: string;
 }
 
-export interface Member {
-  id: number;
-  userId: number;
-  email: string;
-  nickname: string;
-  profileImageUrl: string;
-  createdAt: string;
-  updatedAt: string;
-  isOwner: boolean;
+export interface CreateColumn {
+  title: string;
+  dashboardId: number;
 }
+
+//Comments
 
 export interface Comment {
   id: number;
@@ -71,4 +98,93 @@ export interface Comment {
     nickname: string;
     id: number;
   };
+}
+
+export interface CreateComment {
+  content: string;
+  cardId: number;
+  columnId: number;
+  dashboardId: number;
+}
+
+export interface CheckComment {
+  cursorId: number;
+  comments: Comment[];
+}
+
+export interface ChangeComment {
+  content: string;
+}
+
+export interface CommentList {
+  cursorId: number | null;
+  comments: Comment[];
+}
+
+//Dashboards
+export interface CreateDashBoard {
+  title: string;
+  color: string;
+}
+
+export interface Dashboard {
+  id: number;
+  title: string;
+  color: string;
+  createdAt: string;
+  updatedAt: string;
+  createdByMe: boolean;
+  userId: number;
+}
+
+export interface DashboardList {
+  cursorId: number | null;
+  totalCount: number;
+  dashboards: Dashboard[];
+}
+
+export interface DashboardInvitationList {
+  totalCount: number;
+  invitations: Invitation[];
+}
+
+//Invitations
+export interface Invitation {
+  id: number;
+  inviterUserId: number;
+  teamId: string;
+  dashboard: {
+    title: string;
+    id: number;
+  };
+  invitee: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  inviteAccepted: boolean | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface InvitationList {
+  cursorId: number | null;
+  invitations: Invitation[];
+}
+
+//Members
+export interface MemberList {
+  members: Member[];
+  totalCount: number;
+}
+
+export interface Member {
+  id: number;
+  userId: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+  isOwner: boolean;
 }
