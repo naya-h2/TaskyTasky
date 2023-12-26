ec4f23f5de074a05cb0e973ee11eeb5e4c3a8b94
안희원 <hw7666@sogang.ac.kr>
2023-12-26 21:37:31 +0900

    Feat: 대시보드 생성 기능 완료

--- components/pages/myboard/DashBoardList.tsx
+++ components/pages/myboard/DashBoardList.tsx
@@ -3,39 +3,51 @@ import Button from '@/components/common/Button';
 import { FONT_14, FONT_14_B, FONT_16_B } from '@/styles/FontStyles';
 import { DEVICE_SIZE } from '@/styles/DeviceSize';
 import { BLACK } from '@/styles/ColorStyles';
+import { DashboardType } from '@/lib/types/dashboards';
+import { useStore } from '@/context/stores';
+import DashboardModal from '@/components/common/Modal/DashboardModal';
 
 interface Props {
-  data: any;
+  data: DashboardType[];
 }
 /**
  * @param data dashboard 목록의 배열
  */
 function DashBoardList({ data }: Props) {
+  const { modals, showModal } = useStore((state) => ({ modals: state.modals, showModal: state.showModal }));
+
+  function handleDashboardAdd() {
+    showModal('dashBoard');
+  }
+
   return (
-    <StyledLayout>
-      <StyledBoardList>
-        {data &&
-          data.map((dashboard) => (
-            <StyledButtonWrapper key={dashboard.id}>
-              <Button.DashBoard isOwner={dashboard.createdByMe} chipColor="pink" roundSize="L">
-                <StyledButtonText>{dashboard.title}</StyledButtonText>
-              </Button.DashBoard>
-            </StyledButtonWrapper>
-          ))}
-        <StyledButtonWrapper>
-          <Button.Add roundSize="L">
-            <StyledButtonText>새로운 대시보드</StyledButtonText>
-          </Button.Add>
-        </StyledButtonWrapper>
-      </StyledBoardList>
-      <StyledPagination>
-        <StyledPageInfo>1 페이지 중 1</StyledPageInfo>
-        <StyledMoveButtonWrapper>
-          <Button.Arrow type="left" />
-          <Button.Arrow type="right" />
-        </StyledMoveButtonWrapper>
-      </StyledPagination>
-    </StyledLayout>
+    <>
+      <StyledLayout>
+        <StyledBoardList>
+          {data &&
+            data.map((dashboard) => (
+              <StyledButtonWrapper key={dashboard.id}>
+                <Button.DashBoard isOwner={dashboard.createdByMe} chipColor="pink" roundSize="L">
+                  <StyledButtonText>{dashboard.title}</StyledButtonText>
+                </Button.DashBoard>
+              </StyledButtonWrapper>
+            ))}
+          <StyledButtonWrapper>
+            <Button.Add roundSize="L" onClick={handleDashboardAdd}>
+              <StyledButtonText>새로운 대시보드</StyledButtonText>
+            </Button.Add>
+          </StyledButtonWrapper>
+        </StyledBoardList>
+        <StyledPagination>
+          <StyledPageInfo>1 페이지 중 1</StyledPageInfo>
+          <StyledMoveButtonWrapper>
+            <Button.Arrow type="left" />
+            <Button.Arrow type="right" />
+          </StyledMoveButtonWrapper>
+        </StyledPagination>
+      </StyledLayout>
+      {modals.length > 0 && <DashboardModal type="dashBoard" />}
+    </>
   );
 }
 
