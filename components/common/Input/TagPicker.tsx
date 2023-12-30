0ae4bf825942667a619142322edafc87cf8e970b
이규호 <j980303@naver.com>
2023-12-30 18:35:55 +0900

    Fix: 주석 제거

--- components/common/Input/TagPicker.tsx
+++ components/common/Input/TagPicker.tsx
@@ -1,41 +1,41 @@
-// import { GRAY, VIOLET } from '@/styles/ColorStyles';
-// import TagPicker from 'rsuite/TagPicker';
-// import styled from 'styled-components';
-// import 'rsuite/dist/rsuite.css';
+import { GRAY, VIOLET } from '@/styles/ColorStyles';
+import TagPicker from 'rsuite/TagPicker';
+import styled from 'styled-components';
+import 'rsuite/dist/rsuite.css';
 
-// interface Props {
-//   initialValue?: string[];
-// }
+interface Props {
+  initialValue?: string[];
+}
 
-// function TagPickerCreatable({ initialValue }: Props) {
-//   const data = initialValue
-//     ? initialValue.map((item) => ({
-//         label: item,
-//         value: item,
-//       }))
-//     : [];
+function TagPickerCreatable({ initialValue }: Props) {
+  const data = initialValue
+    ? initialValue.map((item) => ({
+        label: item,
+        value: item,
+      }))
+    : [];
 
-//   return (
-//     <StyledTagPicker
-//       creatable
-//       data={data}
-//       menuStyle={{ height: 120, zIndex: 900, overflowY: 'auto' }}
-//       placeholder="입력 후 Enter"
-//     />
-//   );
-// }
+  return (
+    <StyledTagPicker
+      creatable
+      data={data}
+      menuStyle={{ height: 120, zIndex: 900, overflowY: 'auto' }}
+      placeholder="입력 후 Enter"
+    />
+  );
+}
 
-// export default TagPickerCreatable;
+export default TagPickerCreatable;
 
-// const StyledTagPicker = styled(TagPicker)`
-//   width: 100%;
-//   height: 48px;
-//   display: flex;
-//   align-items: center;
-//   border: 1px solid ${GRAY[30]};
-//   border-radius: 6px;
+const StyledTagPicker = styled(TagPicker)`
+  width: 100%;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  border: 1px solid ${GRAY[30]};
+  border-radius: 6px;
 
-//   &:focus {
-//     border: 1px solid ${VIOLET[1]};
-//   }
-// `;
+  &:focus {
+    border: 1px solid ${VIOLET[1]};
+  }
+`;
