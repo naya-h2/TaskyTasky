57a6ec3b279243071dca8f8336a93483534c2976
이태희 <lte1807@gmail.com>
2024-01-03 12:51:57 +0900

    feat:board/id/edit 삭제기능

--- api/dashboards/deleteDashboardInvitation.ts
+++ api/dashboards/deleteDashboardInvitation.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import authInstance from '@/lib/axios';
 
 /**
  * 대시보드 초대 취소
  */
 export const deleteDashboardInvitation = async (dashboardId: number, invitationId: number) => {
-  const response = await axios.delete(`/api/dashboards/${dashboardId}/invitations/${invitationId}`);
+  const response = await authInstance.delete(`/api/dashboards/${dashboardId}/invitations/${invitationId}`);
 };
