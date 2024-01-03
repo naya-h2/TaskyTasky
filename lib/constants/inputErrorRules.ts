627cf09715cbb7ceb0ab08c44853bf2543790cb0
이규호 <j980303@naver.com>
2024-01-03 13:06:50 +0900

    Feat: SideMenu 기능 수정 및 추가

--- lib/constants/inputErrorRules.ts
+++ lib/constants/inputErrorRules.ts
@@ -11,7 +11,7 @@ export const emailRules = {
 export const nicknameRules = {
   required: ERROR_MSG.emptyNickname,
   pattern: {
-    value: /^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{2,10}$/,
+    value: /^(?=.*[a-zA-Z0-9가-힣 ])[a-zA-Z0-9가-힣 ]{2,10}$/,
     message: ERROR_MSG.invalidNickname,
   },
 };
